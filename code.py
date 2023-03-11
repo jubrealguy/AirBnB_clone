634f3e97600098811cf408ce75390b1827a7d9d2
jubrealguy <whizkidjubrealguy@gmail.com>
2023-03-11 22:42:25 +0000

    restructruring the code

--- code.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-name1 = "Faith"
-name2 = "Adebayo"
-
-print(f"{name1} and {name2} worked on this project")
--- console.py
+++ console.py
@@ -2,25 +2,35 @@
 """
 A program that contains the entry point of the command interpreter
 """
+
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Class HBNBCommand for interpreting """
+    """
+    Class HBNBCommand for interpreting
+    """
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """ To exit the program """
+        """
+        To exit the program
+        """
         return True
 
     def do_EOF(self, arg):
-        """ To exit the program """
+        """
+        To exit the program
+        """
         return True
 
-    def do_emptyline(self):
-        """ When no argument provided """
+    def emptyline(self):
+        """
+        When no argument provided
+        """
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
