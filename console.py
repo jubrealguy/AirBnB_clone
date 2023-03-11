3fbfe43b64e922066c5fec0e27841e2773459361
jubrealguy <whizkidjubrealguy@gmail.com>
2023-03-11 23:17:09 +0000

    updated the console.py

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ A program that contains the entry point of the command interpreter
 
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +13,7 @@ class HBNBCommand(cmd.Cmd):
     Class HBNBCommand for interpreting
     """
     prompt = "(hbnb) "
+    classes = {"BaseModel": BaseModel}
 
     def do_quit(self, arg):
         """
@@ -30,6 +32,51 @@ class HBNBCommand(cmd.Cmd):
         When no argument provided
         """
         pass
+
+    def do_create(self, arg):
+        if not arg:
+            print("** class name missing **")
+        elif arg in classes.keys():
+            obj = self.classes[arg]()
+            obj.save()
+            print(obj.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        args = arg.split(" ")
+        if not arg:
+            print("** class name missing **")
+	elif arg[0] not in self.classes.keys():
+                print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] +'.' + args[1]
+            if key in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+       args = arg.split(" ")
+        if not arg:
+            print("** class name missing **")
+        elif arg[0] not in self.classes.keys():
+                print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] +'.' + args[1]
+            if key in storage.all():
+                del storage.all()[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+
+
 
 
 if __name__ == "__main__":
